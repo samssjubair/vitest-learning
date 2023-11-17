811294e4a5aa113955cd50a793c386db50794f1b
samssjubair <samssjubair@gmail.com>
2023-11-17 17:37:44 +0600

    last commit

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ function myCallback(taskText) {
   console.log(`Task: ${taskText}`);
 }
 
-
 executeCallback(myCallback, "Buy groceries");
 
+export function fetchData() {
+  return new Promise((resolve, reject) => {
+    // Simulate an async operation (e.g., fetching data from an API)
+    setTimeout(() => {
+      const data = { name: "John", age: 30 };
+      resolve(data);
+    }, 1000); // Simulate a 1-second delay
+  });
+}
+
+export const fetchWithError = (key) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (key) {
+        resolve(key);
+      } else {
+        reject(new Error("Error"));
+      }
+    }, 1000);
+  });
+};
--- index.test.js
+++ index.test.js
@@ -1,29 +1,28 @@
-import {it, expect, describe, vi} from 'vitest';
-import { divide, executeCallback } from './index.js';
+import { it, expect, describe, vi } from "vitest";
+import { divide, executeCallback, fetchData, fetchWithError } from "./index.js";
 
+describe("divide()", () => {
+  it("should correctly divides two positive numbers.", () => {
+    const result = divide(6, 3);
+    expect(result).toBe(2);
+  });
 
-describe('divide()', () => {
-    it("should correctly divides two positive numbers.", () => {
-      const result = divide(6, 3);
-      expect(result).toBe(2);
-    });
+  it("should correctly divides two negative numbers.", () => {
+    const result = divide(-6, -3);
+    expect(result).toBe(2);
+  });
 
-    it("should correctly divides two negative numbers.", () => {
-      const result = divide(-6, -3);
-      expect(result).toBe(2);
-    });
-
-    it("should throw a error when divided by zero.", () => {
-      const resultFn = () => {
-        divide(6, 0);
-      };
-      expect(resultFn).toThrowError("Division by zero");
-    });
+  it("should throw a error when divided by zero.", () => {
+    const resultFn = () => {
+      divide(6, 0);
+    };
+    expect(resultFn).toThrowError("Division by zero");
+  });
 });
 
 describe("callback()", () => {
   it("should correctly calls the provided callback function", () => {
-    const loggerFn= vi.fn();
+    const loggerFn = vi.fn();
     executeCallback(loggerFn);
     expect(loggerFn).toHaveBeenCalled();
   });
@@ -33,6 +32,21 @@ describe("callback()", () => {
     executeCallback(loggerFn, "Kire mama");
     expect(loggerFn).toHaveBeenCalledWith("Kire mama");
   });
+});
 
-  
+describe("fetchData()", () => {
+  it("should resolves with the correct data when the asynchronous operation is successful.", async () => {
+    const assumedData = { name: "John", age: 30 };
+    const fetchedData = await fetchData();
+    expect(fetchedData).toStrictEqual(assumedData);
+  });
+
+  it("should correctly rejects the Promise with an error message.", async () => {
+    try {
+        await fetchWithError();
+        expect.fail("Expected an error but got success");
+    } catch (error) {
+        expect(error.message).toBe("Error");
+    }
+  });
 });
